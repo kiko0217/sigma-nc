8571dfda38f12658258cff26fa1b0b6c358c07c7
kiko0217 <anziskita@gmail.com>
2020-11-19 23:02:11 +0700

    perbaikan build

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,3 @@
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-	configureWebpack: config => {
-		return {
-			node: {
-				fs: "empty"
-			}
-		}
-	}
-	// node: {
-	// 	fs: "empty"
-	// }
 }
